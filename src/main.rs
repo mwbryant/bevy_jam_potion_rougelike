2e5f4120a776ab64105c4a271ba21fc970840a62
Matthew <logicprojectsforfpgas@gmail.com>
2022-08-20 13:32:54 -0600

    Basic dodge roll

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{f32::consts::PI, time::Duration};
+
 use bevy::{prelude::*, window::PresentMode};
 use bevy_asset_loader::prelude::*;
 use bevy_inspector_egui::{WorldInspectorParams, WorldInspectorPlugin};
@@ -13,9 +15,14 @@ pub enum GameState {
     Main,
 }
 
-#[derive(Component)]
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
 pub struct Player {
     speed: f32,
+    roll_speed: f32,
+    roll_direction: Vec3,
+    rolling: bool,
+    roll_timer: Timer,
 }
 
 #[derive(Component)]
@@ -62,33 +69,93 @@ fn main() {
             ..Default::default()
         })
         .add_plugin(WorldInspectorPlugin::new())
+        .register_type::<Player>()
         .add_plugin(InputPlugin)
         .add_startup_system(spawn_camera)
         .add_system_set(SystemSet::on_enter(GameState::Main).with_system(spawn_starting_scene))
         .add_system(player_movement)
+        .add_system(player_dodge_roll)
+        .add_system(enemy_movement)
         .run();
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle::default());
 }
-
-fn player_movement(
-    mut player: Query<(&Player, &mut Transform, &ActionState<Action>)>,
+fn player_dodge_roll(
+    mut player: Query<(&mut Player, &mut Transform, &ActionState<Action>)>,
     time: Res<Time>,
 ) {
-    if let Ok((player, mut transform, input)) = player.get_single_mut() {
+    if let Ok((mut player, mut transform, input)) = player.get_single_mut() {
+        //Check input to init roll, also don't roll if no direction
+        if !player.rolling {
+            if input.just_pressed(Action::Roll) && player.roll_direction != Vec3::ZERO {
+                player.rolling = true;
+                player.roll_timer.set_elapsed(Duration::from_secs(0));
+            } else {
+                // Not rolling and not pressing the roll key
+                return;
+            }
+        }
+
+        //Apply roll movement
+        transform.translation += player.roll_speed * player.roll_direction * time.delta_seconds();
+
+        player.roll_timer.tick(time.delta());
+        if player.roll_timer.just_finished() {
+            player.rolling = false;
+            transform.rotation = Quat::from_axis_angle(Vec3::Z, 0.0);
+        } else {
+            //TODO Probably replace with animation
+            transform.rotation =
+                Quat::from_axis_angle(Vec3::Z, 2.0 * PI * player.roll_timer.percent());
+        }
+    }
+}
+
+fn player_movement(
+    mut player: Query<(&mut Player, &mut Transform, &ActionState<Action>)>,
+    time: Res<Time>,
+) {
+    //I'd kinda perfer to crash if theres multiple players but adding a crash isn't that important
+    if let Ok((mut player, mut transform, input)) = player.get_single_mut() {
+        if player.rolling {
+            //Movement locked during roll
+            return;
+        }
+        // Track last movement for roll direction
+        player.roll_direction = Vec3::ZERO;
         if input.pressed(Action::Forward) {
             transform.translation.y += time.delta_seconds() * player.speed;
+            player.roll_direction += Vec3::new(0.0, 1.0, 0.0);
         }
         if input.pressed(Action::Backward) {
             transform.translation.y -= time.delta_seconds() * player.speed;
+            player.roll_direction += Vec3::new(0.0, -1.0, 0.0);
         }
         if input.pressed(Action::Right) {
             transform.translation.x += time.delta_seconds() * player.speed;
+            player.roll_direction += Vec3::new(1.0, 0.0, 0.0);
         }
         if input.pressed(Action::Left) {
             transform.translation.x -= time.delta_seconds() * player.speed;
+            player.roll_direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if player.roll_direction != Vec3::ZERO {
+            player.roll_direction = player.roll_direction.normalize();
+        }
+    }
+}
+
+fn enemy_movement(
+    mut enemy: Query<(&Enemy, &mut Transform), Without<Player>>,
+    player: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(player) = player.get_single() {
+        for (enemy, mut transform) in &mut enemy {
+            let direction = (player.translation - transform.translation).normalize();
+            transform.translation += direction * enemy.speed * time.delta_seconds();
         }
     }
 }
@@ -108,7 +175,13 @@ fn spawn_starting_scene(
             transform: Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::splat(100.)),
             ..default()
         })
-        .insert(Player { speed: 100.0 })
+        .insert(Player {
+            speed: 200.0,
+            roll_speed: 900.0,
+            roll_direction: Vec3::ZERO,
+            rolling: false,
+            roll_timer: Timer::from_seconds(0.4, true),
+        })
         .insert_bundle(InputManagerBundle::<Action> {
             input_map: controls.input.clone(),
             ..default()
